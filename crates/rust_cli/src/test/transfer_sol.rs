e003f274c51a8d6cba2f189bcd773390ee5187c2
alain <zhengze01@gmail>
2024-04-07 18:37:35 +0800

    add transfer_sol test

--- crates/rust_cli/src/test/transfer_sol.rs
+++ crates/rust_cli/src/test/transfer_sol.rs
@@ -1,3 +1,5 @@
+use borsh::BorshSerialize;
+
 #[test]
 pub fn test_lamport_transfer() {
     const RPC_ADDR: &str = "http://127.0.0.1:8899";
@@ -7,32 +9,32 @@ pub fn test_lamport_transfer() {
     use solana_sdk::transaction::Transaction;
     use std::path::PathBuf;
     use std::str::FromStr;
-
+    use transfer_sol::processor::TransferInstruction;
     let transfer_amount: u64 = 1000; // Specify the amount of lamports to transfer
 
-    let program_id = Pubkey::from_str("YourProgramPubkeyHere").unwrap();
+    let program_id = Pubkey::from_str("8czkmWK83eTuhfkYit6rZFMf7yb2nXQxusK5a4JcD2W7").unwrap();
 
-    let keypair_path = PathBuf::from("/path/to/your/keypair.json");
+    let keypair_path = PathBuf::from("/home/pomelo/.config/solana/id.json");
     let payer_keypair =
         read_keypair_file(keypair_path).expect("Failed to read payer keypair from file");
-
-    let recipient_pubkey = Pubkey::from_str("RecipientPubkeyHere").unwrap();
 
     let client = RpcClient::new(RPC_ADDR);
 
     // Create account metas
     let account_metas = vec![
         solana_program::instruction::AccountMeta::new(payer_keypair.pubkey(), true),
-        solana_program::instruction::AccountMeta::new(recipient_pubkey, false),
+        solana_program::instruction::AccountMeta::new(payer_keypair.pubkey(), false),
     ];
 
     // Create instruction
-    let instruction_data = transfer_amount.to_le_bytes(); // Assuming the instruction data just contains the amount
-    let instruction = solana_program::instruction::Instruction::new_with_bytes(
-        program_id,
-        &instruction_data,
-        account_metas,
-    );
+    let mut data = Vec::new();
+    TransferInstruction::ProgramTransfer(transfer_amount)
+        .serialize(&mut data)
+        .unwrap();
+
+    // Assuming the instruction data just contains the amount
+    let instruction =
+        solana_program::instruction::Instruction::new_with_bytes(program_id, &data, account_metas);
 
     // Create and send transaction
     let ixs = vec![instruction];
